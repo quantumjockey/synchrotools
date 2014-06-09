67d138414ded2a350fefcc0ed6508b964e499ebf
Nicola DiPalma <nickyd242@gmail.com>
2014-06-08 21:04:42 -0700

    Added unit test file.
    This will be used to confirm existing functionality now, and for TDD of
    all additional features later.

---
+++ phaseid_spec.py
@@ -0,0 +1,20 @@
+# Description: Allows user to separate data from a synchrotron output file by phase via the terminal.
+# Developer: Nicola B. DiPalma
+# For use with Python v2.7
+
+# Dependencies
+import phaseid
+import unittest
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+	def setUp(self):
+		# test initializers
+
+	def test_shuffle(self):
+		# first test
+
+# Run unit tests
+if __name__ == '__main__':
+	unittest.main()
