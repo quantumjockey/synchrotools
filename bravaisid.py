9badcfa583995742fb5b2cb9726ff7a9912098ab
Nicola DiPalma <nickyd242@gmail.com>
2014-06-27 03:35:39 -0700

    Changed bravais tool description.

--- bravaisid.py
+++ bravaisid.py
@@ -1,4 +1,4 @@
-# Description: Allows user to eliminate mis-measurements from crystal analysis data.
+# Description: Allows user to purge mis-measurements from crystal lattice data.
 # Developer: Nicola B. DiPalma
 # Scripted using Python v3.4.1
 # For use with Python v3.x
