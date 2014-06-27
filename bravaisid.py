f2035bfa0c0991087ee4967e9d556f4308422911
Nicola DiPalma <nickyd242@gmail.com>
2014-06-26 23:48:43 -0700

    First attempt - strange loop.

---
+++ bravaisid.py
@@ -0,0 +1,42 @@
+# Description: Allows user to eliminate mis-measurements from crystal analysis data.
+# Developer: Nicola B. DiPalma
+# Scripted using Python v3.4.1
+# For use with Python v3.x
+
+# Dependencies
+import argparse
+import sys
+
+# script body for file processing
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('infile', type=argparse.FileType('r'), default=sys.stdin, help='File to be parsed.')
+	args = parser.parse_args()
+
+	fileData = args.infile
+	sourceFile = fileData.name
+
+	RemoveEmptyLattices(fileData)
+
+
+# Removes 0,0,0 hkl lattices from the dataset
+def RemoveEmptyLattices(sourceFile):
+	lineOfData = []
+	targetPath = ""
+	lineIndex = 0
+
+	for line in sourceFile:
+		if lineIndex > 1:
+			lineOfData = line.split(',')
+			writeToFile = False
+			for data in lineOfData:
+				if data[0].strip() != "0" and data[1].strip() != "0" and data[2].strip() != "0":
+					print(line)	
+
+		lineIndex += 1
+
+
+# Call main function
+if __name__ == "__main__":
+	main()
---
+++ bravaisid_spec.py
@@ -0,0 +1,21 @@
+# Description: Allows user to eliminate mis-measurements from crystal analysis data.
+# Developer: Nicola B. DiPalma
+# Scripted using Python v3.4.1
+# For use with Python v3.x
+
+# Dependencies
+import bravaisid
+import unittest
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+	def setUp(self):
+		# test initializers
+
+	def test_shuffle(self):
+		# first test
+
+# Run unit tests
+if __name__ == '__main__':
+	unittest.main()
